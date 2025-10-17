8458552e10fa2e3a9e864c12630a4d83b3f48489
Abhishek Shukla <4747uwu@gmail.com>
2025-10-18 03:29:29 +0530

    API

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // Base API URL
-const API_BASE_URL = 'http://localhost:5000/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = '/api';
 
 // Create axios instance for authentication
 export const authAPI = axios.create({
